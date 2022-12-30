4723cd40b6db8399d5a1e7685e12a40697cfcccb
AndersonLoan <97067748+AndersonLoan@users.noreply.github.com>
2022-12-29 21:55:42 -0600

    Add files via upload

---
+++ Howdy World.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Anderson Wayne Loan
+# Section: 574
+# Assignment: Lab 1.9 First Submission 
+# Date: 28/08/2003
+#
+
+print("Howdy, World!")
+print("My favorite ice cream flavor is vanilla with toppings")
